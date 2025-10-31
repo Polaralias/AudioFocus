0140a8f48ec2bcd9fa7498964b5367d846228254
Polaralias <76123410+Polaralias@users.noreply.github.com>
2025-10-31 23:20:48 +0000

    Add AndroidX test core dependency for unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     testImplementation("app.cash.turbine:turbine:1.0.0")
     testImplementation("org.robolectric:robolectric:4.11.1")
+    testImplementation("androidx.test:core-ktx:1.5.0")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
